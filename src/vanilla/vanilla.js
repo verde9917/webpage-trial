08909cf1e03eb980c3b4aeda7388f6eae4c58466
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-06-11 01:21:13 +0900

    complete change CSS function

--- src/vanilla/vanilla.js
+++ src/vanilla/vanilla.js
@@ -1,6 +1,37 @@
-document.getElementById('funcChange').addEventListener('click',changeCSS,false);
-function changeCSS(){
-    console.log("clicked");
+// change first color
+var firstColor = document.getElementById('firstColor');
+firstColor.addEventListener('change',changefirstColor,false);
+function changefirstColor(){
+    var newColor = firstColor.value;
+    document.getElementById('head').style.backgroundColor = newColor;
+    document.body.style.color = newColor;
+    document.getElementById('footer').style.backgroundColor = newColor;
+}
+
+// change second color
+var secondColor = document.getElementById('secondColor');
+secondColor.addEventListener('change',changesecondColor,false);
+function changesecondColor(){
+    var newColor = secondColor.value;
+    document.getElementById('head').style.color = newColor;
+    document.body.style.backgroundColor = newColor;
+    document.getElementById('footer').style.color = newColor;
+}
+
+// count numbers
+var countButton = document.getElementById('countButton');
+countButton.addEventListener('click',countNumber,false);
+function countNumber(){
+    var textNumber = document.getElementById('inputText').value.length;
+    console.log(textNumber);
 }
 
 
+var textresetButton = document.getElementById('textresetButton');
+textresetButton.addEventListener('click',clearText,false);
+function clearText(){
+    var area = document.getElementById('inputText');
+    if(area.value != ''){
+        area.value = '';
+    }
+}
