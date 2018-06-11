f0754453396de41024499e708bb0f7029e7fb608
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-06-11 18:49:13 +0900

    make new Vue() model

--- js/withVue.js
+++ js/withVue.js
@@ -7,16 +7,26 @@ new Vue({
     },
     methods: {
         changefirst: function () {
-            var newColor = this.firstcolorID
+            var newColor = this.firstcolorID;
             $('body').css('color', newColor);
             $('#head').css('background-color', newColor);
             $('#footer').css('background-color', newColor);
         },
         changesecond: function () {
-            var newColor = this.secondcolorID
+            var newColor = this.secondcolorID;
             $('body').css('background-color', newColor);
             $('#head').css('color', newColor);
             $('#footer').css('color', newColor);
         },
     }
+}, {
+    el: "#inputText",
+    data: {
+        string: '',
+        number: 0,
+    },
+    methods: {
+        countChar: function () {},
+        resetChar: function () {}
+    },
 })
